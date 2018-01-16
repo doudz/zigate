6edd0c57e9ee817f14f0d056ae229d5b6247b28a
Sébastien RAMAGE <sebastien.ramage@gmail.com>
2018-01-16 11:02:08 +0100

    update setup

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3
-
-from setuptools import setup, find_packages
+from distutils.core import setup
 import zigate
- 
+
 setup(
     name='zigate',
     version=zigate.__version__,
-    description='python library for the zigate gateway',
+    description='python library for the zigate gateway (zigbee) http://zigate.fr',
     long_description=open('README.md').read(),
     author='Sébastien RAMAGE',
     author_email='sebastien.ramage@gmail.com',
     url='https://github.com/doudz/zigate',
     packages=['zigate'],
-    keywords='zigate zigbee',
-    install_requires=['pyserial'],
+    keywords='zigate zigbee python3',
+    install_requires=['pyserial', 'pyudev'],
     python_requires='>=3',
     classifiers=(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3',
     ),
 )
