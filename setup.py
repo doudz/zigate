1b7459ae30fb19fedc6f8e57ddb5e6bce86e6080
Guillaume Kulakowski <guillaume@kulakowski.fr>
2018-09-27 21:28:43 +0200

    Add tox support:  - tox.ini  - setup.cfg  - setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,54 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2018 Sébastien RAMAGE
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+
+"""
+zigate, setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
 from setuptools import setup
 import zigate
 
+# Setup part
 setup(
     name='zigate',
     version=zigate.__version__,
     description='python library for the zigate gateway (zigbee) http://zigate.fr',
     long_description=open('README.rst').read(),
+    url='https://github.com/doudz/zigate',
     author='Sébastien RAMAGE',
     author_email='sebastien.ramage@gmail.com',
-    url='https://github.com/doudz/zigate',
-    packages=['zigate'],
-    keywords='zigate zigbee python3',
-    install_requires=['pyserial', 'pydispatcher'],
-    python_requires='>=3',
+
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3',
-        "License :: OSI Approved :: MIT License",
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
     ],
+
+    keywords='zigate zigbee python3',
+    packages=['zigate'],
+
+    install_requires=[
+        'pyserial',
+        'pydispatcher'
+    ],
+    extras_require={
+        'dev': ['tox']
+    },
+    python_requires='>=3',
+
+    project_urls={
+        'Bug Reports': 'https://github.com/doudz/zigate/issues',
+        'Source': 'https://github.com/doudz/zigate/',
+    },
 )
