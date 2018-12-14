af207e5d72b2e5914647a015cee4df6bb7305635
Sébastien RAMAGE <sebastien.ramage@gmail.com>
2018-12-14 16:21:42 +0100

    Begin some transport test

---
+++ tests/test_transport.py
@@ -0,0 +1,33 @@
+'''
+ZiGate Transport Tests
+-------------------------
+'''
+
+import unittest
+from zigate import transport
+from collections import OrderedDict
+
+
+class TestTransport(unittest.TestCase):
+    def test_packet(self):
+        connection = transport.BaseTransport()
+        data = b'\x01123\x03'
+        connection.read_data(data)
+        self.assertEqual(data, connection.received.get())
+        print(connection._buffer)
+        data = b'123\x03'
+        connection.read_data(data)
+#         self.assertEqual(data, connection.received.get())
+        print(connection._buffer)
+        data = b'\x01123'
+        connection.read_data(data)
+#         self.assertEqual(data, connection.received.get())
+        print(connection._buffer)
+        data = b'123\x01123\x03'
+        connection.read_data(data)
+#         self.assertEqual(data, connection.received.get())
+        print(connection._buffer)
+
+
+if __name__ == '__main__':
+    unittest.main()
