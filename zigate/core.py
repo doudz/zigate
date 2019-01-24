29e5b237c386af7ac7fdc40c24db906400ea5fd2
Sébastien RAMAGE <sebastien.ramage@gmail.com>
2019-01-24 09:36:20 +0100

    setTime and getTime are related to 2000 not 2001 https://github.com/fairecasoimeme/ZiGate/issues/124

--- zigate/core.py
+++ zigate/core.py
@@ -324,7 +324,7 @@ class ZiGate(object):
 
         if version['version'] >= '3.0f':
             LOGGER.debug('Set Zigate Time (firmware >= 3.0f)')
-            self.setTime()
+            self.set_time()
         self.get_devices_list(True)
         self.need_discovery()
 
@@ -765,18 +765,18 @@ class ZiGate(object):
             r = r.get('status', False)
         return r
 
-    def setTime(self, dt=None):
+    def set_time(self, dt=None):
         '''
         Set internal zigate time
         dt should be datetime.datetime object
         '''
         dt = dt or datetime.datetime.now()
-        # timestamp from 2001-01-01 00:00:00
-        timestamp = int((dt - datetime.datetime(2001, 1, 1)).total_seconds())
+        # timestamp from 2000-01-01 00:00:00
+        timestamp = int((dt - datetime.datetime(2000, 1, 1)).total_seconds())
         data = struct.pack('!L', timestamp)
         self.send_data(0x0016, data)
 
-    def getTime(self):
+    def get_time(self):
         '''
         get internal zigate time
         '''
@@ -784,7 +784,7 @@ class ZiGate(object):
         dt = None
         if r:
             timestamp = r.get('timestamp')
-            dt = datetime.datetime(2001, 1, 1) + datetime.timedelta(seconds=timestamp)
+            dt = datetime.datetime(2000, 1, 1) + datetime.timedelta(seconds=timestamp)
         return dt
 
     def permit_join(self, duration=30):
