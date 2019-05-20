913a886e9b0bafeadd5d80a9905d54db73e8c35a
Sébastien RAMAGE <sebastien.ramage@gmail.com>
2019-05-20 12:16:01 +0200

    Add fake thermostat actions

--- zigate/core.py
+++ zigate/core.py
@@ -2198,6 +2198,8 @@ class Device(object):
                         actions[ep_id].append(ACTIONS_LOCK)
                     if 0x0102 in endpoint['in_clusters']:
                         actions[ep_id].append(ACTIONS_COVER)
+                    if 0x0201 in endpoint['in_clusters']:
+                        actions[ep_id].append(ACTIONS_THERMOSTAT)
                     if 0x0300 in endpoint['in_clusters']:
                         # if endpoint['device'] in (0x0102, 0x0105):
                         if endpoint['device'] in (0x0105,):
