c3ba5b2fda392af9e4308b9bf83a1c2af311f0f5
Sébastien RAMAGE <sebastien.ramage@gmail.com>
2019-06-19 08:58:27 +0200

    Add quick and dirty ikea ota converter

---
+++ ikea_ota_converter.py
@@ -0,0 +1,26 @@
+'''
+Created on 19 juin 2019
+
+@author: doudz
+
+IKEA OTA files are signed
+this script just remove the digital signature
+'''
+import sys
+import struct
+
+path = sys.argv[1]
+
+with open(path, 'rb') as fp:
+    data = fp.read()
+
+if not data.startswith(b'NGIS'):
+    raise Exception('Not a signed file, no need to convert')
+
+print('Converting', path)
+header_end = struct.unpack('<I', data[0x10:0x14])[0]+1
+footer_pos = struct.unpack('<I', data[0x18:0x1C])[0]
+data = data[header_end:footer_pos]
+with open(path, 'wb') as fp:
+    fp.write(data)
+print(path, 'converted')
