41f33abd96714d512afa43db21901b8c70bed866
Sébastien RAMAGE <sebastien.ramage@gmail.com>
2020-01-31 10:31:32 +0100

    update test

--- tests/test_devices.py
+++ tests/test_devices.py
@@ -226,12 +226,8 @@ class TestCore(unittest.TestCase):
         self.assertEqual(device.get_property_value('onoff'), True)
         device.set_attribute(1, 0x0006, {'attribute': 0x0000, 'lqi': 255, 'data': False})
         self.assertEqual(device.get_property_value('onoff'), True)
-        time.sleep(core.DELAY_FASTCHANGE + 1)
+        time.sleep(core.DELAY_FASTCHANGE)
         self.assertEqual(device.get_property_value('onoff'), False)
-        device.set_attribute(1, 0x0006, {'attribute': 0x0000, 'lqi': 255, 'data': True})
-        self.assertEqual(device.get_property_value('onoff'), False)
-        time.sleep(core.DELAY_FASTCHANGE + 1 )
-        self.assertEqual(device.get_property_value('onoff'), True)
 
 
 if __name__ == '__main__':
